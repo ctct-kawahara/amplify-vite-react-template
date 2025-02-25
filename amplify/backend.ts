1197cc31db0448326132ddf07fe0f31d15fe6f55
CTCT-Kawahwara <148046448+ctct-kawahara@users.noreply.github.com>
2025-02-25 10:56:33 +0900

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,13 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+  import { defineBackend } from '@aws-amplify/backend';
 
-defineBackend({
-  auth,
-  data,
-});
++ import { auth } from './auth/resource';
+
+
+
+- defineBackend({});
+
++ defineBackend({
+
++     auth,
+
++ });
