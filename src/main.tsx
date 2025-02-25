fd49607ac57419c4e13fa7e5a894fdf9cf86fee0
CTCT-Kawahwara <148046448+ctct-kawahara@users.noreply.github.com>
2025-02-25 11:02:02 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+  import React from 'react'
+  import ReactDOM from 'react-dom/client'
+  import App from './App.tsx'
+  import './index.css'
+  
 
-Amplify.configure(outputs);
++ import '@aws-amplify/ui-react/styles.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  
+
++ import { Amplify } from 'aws-amplify'
+
++ import outputs from '../amplify_outputs.json'
+
+  
+
++ Amplify.configure(outputs)
+
+  
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
